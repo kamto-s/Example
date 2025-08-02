a8462f5940a0af615acee58a57889632a3ffb1dc
kamto-s <kamto.076s@gmail.com>
2025-08-02 09:42:48 +0700

    Add project files.

---
+++ Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 5 + A;
+            Console.WriteLine(B * B);
+        }
+    }
+}
